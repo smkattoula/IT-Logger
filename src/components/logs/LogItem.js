12f8078417c69a5e87a0f43455a786cb243ffb07
Shaker Kattoula <smkattoula@gmail.com>
2020-11-02 15:34:26 -0800

    create PUT fetch request to update log fields in the database

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Moment from "react-moment";
 import { connect } from "react-redux";
-import { deleteLog } from "../../actions/logActions";
+import { deleteLog, setCurrent } from "../../actions/logActions";
 import PropTypes from "prop-types";
 
 import M from "materialize-css/dist/js/materialize.min.js";
 
-const LogItem = ({ log, deleteLog }) => {
+const LogItem = ({ log, deleteLog, setCurrent }) => {
   const onDelete = () => {
     deleteLog(log.id);
     M.toast({ html: "Log Deleted" });
@@ -21,6 +21,7 @@ const LogItem = ({ log, deleteLog }) => {
             className={`modal-trigger ${
               log.attention ? "red-text" : "blue-text"
             }`}
+            onClick={() => setCurrent(log)}
           >
             {log.message}
           </a>
@@ -42,6 +43,7 @@ const LogItem = ({ log, deleteLog }) => {
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
   deleteLog: PropTypes.func.isRequired,
+  setCurrent: PropTypes.func.isRequired,
 };
 
-export default connect(null, { deleteLog })(LogItem);
+export default connect(null, { deleteLog, setCurrent })(LogItem);
